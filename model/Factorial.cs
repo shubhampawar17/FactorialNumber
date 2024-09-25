4f460e9ce9a6c97a721e5b2781f6713b8cd20a2f
shubhampawar17 <sp1180253@gmail.com>
2024-09-25 14:27:28 +0530

    Add project files.

---
+++ model/Factorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialNumber.model
+{
+    internal class Factorial
+    {
+        public static long CalculateFactorial(int num)
+        {
+            long fact = 1;
+            for (int i = num; i >= 1; i--)
+            {
+                fact = fact * i;
+            }
+            return fact;
+        }
+    }
+}
